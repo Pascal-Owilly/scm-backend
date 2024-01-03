97d29104b13bf93bf096c6e03243692010400bff
Pascal Owilly <owillypascal@gmail.com>
2024-01-03 11:44:45 +0300

    update payment

--- custom_registration/admin.py
+++ custom_registration/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from custom_registration.models import CustomUser
+from custom_registration.models import CustomUser, Bank, BankBranch, Status, Payment
 
 admin.site.register(CustomUser)
+admin.site.register(Bank)
+admin.site.register(BankBranch)
+admin.site.register(Status)
+admin.site.register(Payment)
